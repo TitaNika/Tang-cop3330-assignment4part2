46c09018dffe64b094f92a5dac0048ec6eb7e18e
TitaNika <tonytang@knights.ucf.edu>
2021-11-01 19:05:06 -0400

    Initial commit

---
+++ src/test/java/HelloApplicationTest.java
@@ -0,0 +1,15 @@
+class HelloApplicationTest {
+
+
+    @org.junit.jupiter.api.Test
+    void start() {
+        //
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void main() {
+        //
+    }
+
+}
